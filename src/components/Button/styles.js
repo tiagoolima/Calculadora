0c40a2eef2e5dd448b892a98fd921d56f7cf1213
Tiago <tiagotome38@gmail.com>
2024-03-21 20:59:29 -0300

    first commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components"
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #AAAAAA;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
+
+export const ButtonRight = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #F4A460;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+
+    &:hover {
+        opacity: 0.6;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #4F4F4F;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  font-size: 24px;
+  font-family: Roboto;
+
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color: #4F4F4F;
+    border: 0;
+    padding: 0 10px;
+    font-size: 24px;
+    font-family: Roboto;
+  }
+`
